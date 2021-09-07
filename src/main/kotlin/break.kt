3e3a763b4a7cdad2d25c8d174ddf41858faaa399
abdkarim1229 <abdulkarimklj@gmail.com>
2021-09-07 13:35:37 +0700

    commit-tugas5

--- src/main/kotlin/break.kt
+++ src/main/kotlin/break.kt
@@ -1,9 +1,13 @@
 fun main() {
+    //membuat variabel array "Buah"
     var Buah = arrayOf("Semangka", "Anggur", "Rambutan", "Apel", "Mangga", "Alpukat", "Pisang")
     println("Mencari Buah Mangga di Array Buah :")
-    for (buah in Buah){
+    //menerapkan perulangan "for loop" untuk menampilkan array buah
+    for (buah in Buah) {
         println(buah)
-        if (buah == "Mangga"){
+        /*menggunakan "if" untuk mencari buah Mangga dan menggunakan
+        "break statement" jika buah mangga sudah di temukan maka pencarian akan selesai*/
+        if (buah == "Mangga") {
             println("Buah ($buah) Sudah di temukan ")
             break
         }
--- src/main/kotlin/break_continue.kt
+++ src/main/kotlin/break_continue.kt
@@ -1,7 +1,10 @@
 fun main() {
+    //menerapkan perulangan for untuk menampilkan angka 1 sampai 20
     for (nomer in 1..20) {
+        //menggunakan if, modulus 2 dan continue statament
         if (nomer % 2 == 0)
             continue
+        //menggunakan if, dan break > 15 untuk menampilkan output sampai 15 saja
         if (nomer > 15) {
             break
         }
